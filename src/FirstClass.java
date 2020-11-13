8c401eb37b124a728224c06df399746912220058
ATX1 <ATX1@Mahfuz>
2020-11-12 20:21:32 -0500

    First class is updated.

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,6 +2,7 @@
 public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Learning GitHub");
+		System.out.println("Updating the latest class with latest information");
 	}
 
 }
